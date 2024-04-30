35edeb77b8274f94102171512b2023fc0b251cb4
AdrianSote <adrianmvparedes@gmail.com>
2024-04-30 21:00:16 +0200

    Generating first validators to routes

---
+++ ApiRest/src/valdiators/users.js
@@ -0,0 +1,27 @@
+const { check } = require("express-validator");
+const validateResults = require("../utils/handleValidator");
+
+const validatorCreateUser = [
+    check("DNI").exists().notEmpty().isString(),
+    check("Rol").exists().notEmpty().isIn(['Participante', 'Monitor', 'Coordinador']),
+    check("Name").exists().notEmpty().isString(),
+    check("Surname_1").exists().notEmpty().isString(),
+    check("Surname_2").exists().notEmpty().isString(),
+    check("Email").exists().notEmpty().isEmail(),
+    check("Pass").exists().notEmpty().isString(),
+    check("Photo").optional().isString(),
+    check("DNI_tutor").optional().custom((value, { req }) => {
+        if (value !== null) {
+            // Si el valor no es null, debe ser una cadena de hasta 10 caracteres
+            return typeof value === 'string' && value.length <= 10;
+        }
+        return true; // Si es null, la validación pasa
+    }).withMessage('DNI_tutor debe ser una cadena de hasta 10 caracteres si se proporciona'),
+    check("Adress").exists().notEmpty().isString(),
+    check("Phone").exists().notEmpty().isString(),
+    (req, res, next) => {
+        return validateResults(req, res, next);
+    }
+];
+
+module.exports = { validatorCreateUser };
