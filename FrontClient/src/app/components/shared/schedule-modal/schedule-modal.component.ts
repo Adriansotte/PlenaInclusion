766b7d2c0a637a82c26e8460514baabc5ca9f265
AdrianSote <adrianmvparedes@gmail.com>
2024-05-24 12:28:37 +0200

    Apuntarse a actividad con varias fechas y con una sola fecha hechio

--- FrontClient/src/app/components/shared/schedule-modal/schedule-modal.component.ts
+++ FrontClient/src/app/components/shared/schedule-modal/schedule-modal.component.ts
@@ -12,6 +12,8 @@ import { getWeeklyAttendanceDates } from '../../../../utils/getWeeklyAttendanceD
 export class ScheduleModalComponent {
   @Input() schedule: scheduleDTO | null = null;
 
+  fechas: string[] = [];
+
   constructor(private userScheduleService: UserScheduleService) { }
 
   registerForSchedule() {
@@ -19,29 +21,42 @@ export class ScheduleModalComponent {
       const userId = sessionStorage.getItem('ID_User');
       if (userId) {
         const scheduleId = this.schedule.ID_Schedule;
+        this.calcularFechas();
+        for (const date of this.fechas) {
+          this.launchInsertion(userId, scheduleId, date)
+        }
 
-        this.userScheduleService.postSchedule(userId, scheduleId, "1999-09-25").subscribe({
-          next: response => {
-            console.log('Registro exitoso', response);
-          },
-          error: error => {
-            console.error('Error en el registro', error);
-          },
-          complete: () => {
-            console.log('Registro completado');
-          }
-        });
       } else {
         console.error('ID de usuario no encontrado en el sesionStorage');
       }
     } else {
       console.error('No hay schedule seleccionado');
     }
+    this.fechas = [];
+
+  }
+
+  launchInsertion(userId: string, scheduleId: string, date: string) {
+    this.userScheduleService.postSchedule(userId, scheduleId, date).subscribe({
+      next: response => {
+        console.log('Registro exitoso', response);
+      },
+      error: error => {
+        console.error('Error en el registro', error);
+      },
+      complete: () => {
+        console.log('Registro completado');
+      }
+    });
   }
 
   calcularFechas() {
     if (this.schedule && this.schedule.Frequency === "Semanal") {
-      const fechas = getWeeklyAttendanceDates(this.schedule.StartDate, this.schedule.FinishDate);
+      this.fechas = getWeeklyAttendanceDates(this.schedule.StartDate, this.schedule.FinishDate);
+    } else {
+      this.fechas.push(new Date(this.schedule?.StartDate!).toISOString().split('T')[0]);
     }
+    console.log(this.fechas)
+    //TODO: dejar en vacio las fechas despues de asignar
   }
 }
--- FrontClient/src/utils/getWeeklyAttendanceDates.ts
+++ FrontClient/src/utils/getWeeklyAttendanceDates.ts
@@ -4,13 +4,9 @@ export function getWeeklyAttendanceDates(startDate: Date, endDate: Date): string
     let finishDate = new Date(endDate);
     currentDate.setHours(0, 0, 0, 0);
     finishDate.setHours(0, 0, 0, 0);
-    console.log("Initial currentDate:", currentDate.getDay());
-
     while (currentDate <= finishDate) {
         dates.push(currentDate.toISOString().split('T')[0]);
         currentDate.setDate(currentDate.getDate() + 7);
     }
-
-    console.log("Final dates array:", dates);
     return dates;
 }
