ce77943ffbcfe8bea106f8421d07eb217f342787
AdrianSote <adrianmvparedes@gmail.com>
2024-05-24 13:19:44 +0200

    arreglado problema de asignacion de horario

--- FrontClient/src/utils/getWeeklyAttendanceDates.ts
+++ FrontClient/src/utils/getWeeklyAttendanceDates.ts
@@ -2,10 +2,12 @@ export function getWeeklyAttendanceDates(startDate: Date, endDate: Date): string
     const dates: string[] = [];
     let currentDate = new Date(startDate);
     let finishDate = new Date(endDate);
-    currentDate.setHours(0, 0, 0, 0);
-    finishDate.setHours(0, 0, 0, 0);
+    let nowADay = new Date();
+    nowADay.setHours(1, 0, 0, 0);
     while (currentDate <= finishDate) {
-        dates.push(currentDate.toISOString().split('T')[0]);
+        if (currentDate >= nowADay) {
+            dates.push(currentDate.toISOString().split('T')[0]);
+        }
         currentDate.setDate(currentDate.getDate() + 7);
     }
     return dates;
