b5c1f7f9ae7d4aecb95ed77f5403d12c6c9b8598
AdrianSote <adrianmvparedes@gmail.com>
2024-05-31 10:01:57 +0200

    Agregados Modales de confirmacion

--- FrontClient/src/app/components/shared/campagne-modal/campagne-modal.component.ts
+++ FrontClient/src/app/components/shared/campagne-modal/campagne-modal.component.ts
@@ -2,6 +2,8 @@ import { Component, EventEmitter, Input, Output } from '@angular/core';
 import { CampaignDTO } from 'src/app/models/campaign/campaignDTO';
 import { CampaignService } from 'src/app/services/campaign/campaign.service';
 
+declare var bootstrap: any;
+
 @Component({
   selector: 'app-campagne-modal',
   templateUrl: './campagne-modal.component.html',
@@ -13,23 +15,45 @@ export class CampagneModalComponent {
 
   @Output() campaignsChange: EventEmitter<CampaignDTO> = new EventEmitter<CampaignDTO>();
 
+  adviceTitle: string = "";
+
   constructor(private campaignService: CampaignService) { }
 
-  deleteActivity() {
+  deleteCampagne() {
     this.campaignService.deleteCampaign(this.selectedCampaign?.ID_Campaign!).subscribe({
       next: response => {
-        console.log(response)
+        this.adviceTitle = "Campaña borrada correctamente!!!";
 
         // this.deleteSuccessModal();
       },
       error: (error: any) => {
         console.log(error)
+        this.adviceTitle = "Campaña Pertenece a un Horario, de momento no se puede borrar.";
+        this.openAdviceModal();
 
         // this.deleteFailedModal()
       },
       complete: () => {
         this.campaignsChange.emit();
+        this.openAdviceModal();
+
       }
     })
+  }
+
+  openConfirmationModal() {
+    const modalElement = document.getElementById('confirmationModal');
+    if (modalElement) {
+      const modal = new bootstrap.Modal(modalElement);
+      modal.show();
+    }
+  }
+
+  openAdviceModal() {
+    const modalElement = document.getElementById('successModal');
+    if (modalElement) {
+      const modal = new bootstrap.Modal(modalElement);
+      modal.show();
+    }
   }
 }
