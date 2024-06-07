3f3c74bcc668114f039807a9570a995b118e0dfc
AdrianSote <adrianmvparedes@gmail.com>
2024-06-07 21:46:39 +0200

    Comentarios

--- FrontClient/src/app/components/shared/profile-info-modal/profile-info-modal.component.ts
+++ FrontClient/src/app/components/shared/profile-info-modal/profile-info-modal.component.ts
@@ -1,5 +1,8 @@
-import { Component, Input } from '@angular/core';
+import { Component, EventEmitter, Input, Output } from '@angular/core';
 import { UserDTO } from 'src/app/models/user/userDTO';
+import { UserService } from 'src/app/services/user/user.service';
+
+declare var bootstrap: any;
 
 @Component({
   selector: 'app-profile-info-modal',
@@ -9,4 +12,26 @@ import { UserDTO } from 'src/app/models/user/userDTO';
 export class ProfileInfoModalComponent {
 
   @Input() selectedUser: UserDTO | null = null;
+  @Output() userDeleted: EventEmitter<void> = new EventEmitter<void>();
+
+  constructor(private userService: UserService) { }
+
+  deleteUser() {
+    this.userService.deleteUser(this.selectedUser?.ID_user!).subscribe({
+      complete: () => {
+        this.openAdviceModal();
+        this.userDeleted.emit();
+      }
+    })
+  }
+
+  openAdviceModal() {
+    const modalElement = document.getElementById('adviceConfirmation');
+    if (modalElement) {
+      const modal = new bootstrap.Modal(modalElement);
+      modal.show();
+    }
+  }
+
+
 }
